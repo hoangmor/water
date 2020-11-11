867ec9460417f8e21f1748ea580e817e00afbcaa
HoangNguyen <hoang.nguyenxuan@mor.com.vn>
2020-11-11 17:39:53 +0700

    update

--- app/Http/Controllers/WellcomeController.php
+++ app/Http/Controllers/WellcomeController.php
@@ -37,6 +37,22 @@ class WellcomeController extends Controller
                 $arrDataArea[$id]['rest'] = $dataMaxArea[$time];
             }
         }
+        //get khung gio du thuyen
+        $arrTimeChooseShip=[
+                        'min_1' => '11:30 - 11:50',
+                        'min_2' => '12:00 - 12:20',
+                        'min_3' => '12:30 - 12:50',
+                        'min_4' => '03:00 - 03:20',
+                        'min_5' => '03:00 - 03:40',
+                        'min_6' => '03:50 - 04:10',
+                        'min_7' => '04:20 - 04:40',
+                        'min_8' => '04:50 - 05:00',
+                        'min_9' => '05:10 - 05:30',
+                        'min_10' => '05:50 - 05:30',
+        ];
+        $dataTimeShip = Customer::where('type', 1)->get()->groupBy('time_start')->toArray();
+        $arrTimeEndShip = ['11:50'=>'min_1', '12:20' => 'min_2', '12:50' => 'min_3', '3:20' => 'min_4', '3:40' => 'min_5', '4:10' => 'min_6', '4:40' => 'min_7', '5:00' => 'min_9', '5:30' => 'min_9', '6:10' => 'min_10'];
+
 
         $codeArr = ['AAA', 'BBB', 'CCC'];
         return view('welcome', compact('codeArr', 'arrLunchTime', 'arrDataArea'));
@@ -54,8 +70,8 @@ class WellcomeController extends Controller
         $data['time_start'] = $arrTimeStartLunch[$request->Time];
         $data['time_end'] = $arrTimeEndLunch[$request->Time];
         $data['date'] = $now->format('Y-m-d');
-        $data['created_at'] = $now->format('Y-m-d H:i:s');   
-        $data['updated_at'] = $now->format('Y-m-d H:i:s'); 
+        $data['created_at'] = $now->format('Y-m-d H:i:s');
+        $data['updated_at'] = $now->format('Y-m-d H:i:s');
         $data['type'] = 1;
         $newData = Customer::create($data);
         return ['id' => $newData->id, 'area_cuss' => $newData->area, 'start_cuss' => $newData->time_start, 'end_cuss' => $newData->time_end];
@@ -73,8 +89,8 @@ class WellcomeController extends Controller
         $data['time_start'] = '11:30';
         $data['time_end'] = '11:30';
         $data['date'] = $now->format('Y-m-d');
-        $data['created_at'] = $now->format('Y-m-d H:i:s');   
-        $data['updated_at'] = $now->format('Y-m-d H:i:s'); 
+        $data['created_at'] = $now->format('Y-m-d H:i:s');
+        $data['updated_at'] = $now->format('Y-m-d H:i:s');
         $data['type'] = 2;
         $newData = Customer::create($data);
         return ['id' => $newData->id, 'area_cuss' => $newData->area, 'start_cuss' => $newData->time_start, 'end_cuss' => $newData->time_end];
